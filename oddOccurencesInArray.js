788395ffabfa0b211b6c18a05fe0d9a5c04bcd12
Matt <43910247+mattrobthomas@users.noreply.github.com>
2019-11-10 16:49:21 +0000

    Too many parentheses

--- oddOccurencesInArray.js
+++ oddOccurencesInArray.js
@@ -1,11 +1,13 @@
 // This code finds the only number from an array that does not have an equal match.
-// It sorts the array from low to high, and then looks to see which which neighbouring number does not match. i+=2 allows the for loop to go two numbers ahead if i == i+1. if i is not == to i+1 the loops returns the current number.
+// It sorts the array from low to high, and then looks to see which which neighbouring number does not match. 
+// i+=2 allows the for loop to go two numbers ahead if i == i+1. 
+// if i is not == to i+1 the loops returns the current number.
 
 function solution(A) {
   A.sort(); 
     
   for (var i = 0; i < A.length; i+=2) {
-    if ((A[i] !== A[i+1])) 
+    if (A[i] !== A[i+1]) 
     return A[i];  
   } 
 }
